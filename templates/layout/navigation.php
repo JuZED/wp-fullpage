ba186dbe8c7566f6dd2932e24adda242cef6f966
JuZED <or.n.juz@gmail.com>
2014-07-11 18:54:04 +0200

    Comment upated

--- templates/layout/navigation.php
+++ templates/layout/navigation.php
@@ -1,9 +1,7 @@
 <?php
+
 /**
  * The template for displaying the Fullpage Navigation
- *
- * Contains footer content.
- *
  */
 ?>
 
@@ -20,7 +18,7 @@
 			foreach( WPFP_Query()->sections as $key => $section ) :
 
 			?>
-		
+
 				<li class="<?php print $key === 0 ? 'active' : ''; ?>" data-menuanchor="<?php print $section->post_name ?>" data-tooltip="<?php print esc_html( WPFP_Query()->get_section_nav_title( $key ) ); ?>">
 					<a href="#<?php print $section->post_name ?>">
 						<span></span>
