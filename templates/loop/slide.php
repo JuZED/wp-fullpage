ee5ed3ac9aa1a0f321cb7be8a0b5ec39c60daab2
JuZED <or.n.juz@gmail.com>
2014-07-12 12:09:06 +0200

    Adding get_slide_position in slide tpl

--- templates/loop/slide.php
+++ templates/loop/slide.php
@@ -9,66 +9,44 @@
 
 ?>
 
-<?php
+<div class="slide fp-table <?php WPFP_Query()->get_slide_position(); ?>" data-anchor="<?php print WPFP_Query()->slide->post_name; ?>" data-bg="<?php print WPFP_Query()->get_slide_bg(); ?>">
 	
-	$slide_count = WPFP_Query()->section->slide_count;
-	
-	if( 1 < $slide_count ) :
+	<div class="fp-tableCell">
 
-?>
+		<article <?php post_class( '', WPFP_Query()->get_slide_ID() ); ?>>
 
-<div class="slide" data-anchor="<?php print WPFP_Query()->slide->post_name; ?>" data-bg="<?php print WPFP_Query()->get_slide_bg(); ?>">
+			<header>
+				
+				<div class="entry-meta icon">
+					<span class="section-link">
+						<a href="#<?php print WPFP_Query()->section->post_name; ?>" title="<?php print esc_attr( WPFP_Query()->get_section_title( 0, '', '', false ) ); ?>">
+							<?php WPFP_Query()->get_section_title(); ?>
+						</a>
+					</span>
+				</div>
 
-<?php
+				<?php WPFP_Query()->get_slide_title( 0, '<h2 class="entry-title icon">', '</h2>' ); ?>
+				
+				<div class="entry-meta">
+					<span class="entry-date">
+						<time class="entry-date" datetime="<?php print esc_attr( WPFP_Query()->get_slide_date( 0, 'c' ) ); ?>">
+							<?php WPFP_Query()->get_slide_date(); ?>
+						</time>
+					</span>&nbsp;
+					<span class="entry-author vcard">
+						<?php WPFP_Query()->get_slide_author(); ?>
+					</span>&nbsp;
+					<?php edit_post_link( __( 'Edit', WPFP_DOMAIN ), '<span class="edit-link">', '</span>', WPFP_Query()->get_slide_ID() ); ?>
+				</div>
 
-	endif;
+			</header>
 
-?>
-
-	<article <?php post_class( '', WPFP_Query()->get_slide_ID() ); ?>>
-
-		<header>
-			
-			<div class="entry-meta icon">
-				<span class="section-link">
-					<a href="#<?php print WPFP_Query()->section->post_name; ?>" title="<?php print esc_attr( WPFP_Query()->get_section_title( 0, '', '', false ) ); ?>">
-						<?php WPFP_Query()->get_section_title(); ?>
-					</a>
-				</span>
+			<div class="entry-content">
+				<?php WPFP_Query()->get_content(); ?>
 			</div>
 
-			<?php WPFP_Query()->get_slide_title( 0, '<h2 class="entry-title icon">', '</h2>' ); ?>
-			
-			<div class="entry-meta">
-				<span class="entry-date">
-					<time class="entry-date" datetime="<?php print esc_attr( WPFP_Query()->get_slide_date( 0, 'c' ) ); ?>">
-						<?php WPFP_Query()->get_slide_date(); ?>
-					</time>
-				</span>&nbsp;
-				<span class="entry-author vcard">
-					<?php WPFP_Query()->get_slide_author(); ?>
-				</span>&nbsp;
-				<?php edit_post_link( __( 'Edit', WPFP_DOMAIN ), '<span class="edit-link">', '</span>', WPFP_Query()->get_slide_ID() ); ?>
-			</div>
+		</article>
 
-		</header>
-
-		<div class="entry-content">
-			<?php WPFP_Query()->get_content(); ?>
-		</div>
-
-	</article>
-
-<?php
-
-	if( 1 < $slide_count ) :
-
-?>
+	</div><!-- .fp-tableCell -->
 
 </div><!-- .slide -->
-
-<?php
-
-	endif;
-
-?>
