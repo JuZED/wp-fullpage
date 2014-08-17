12cf25d64329113811a53f343d72c26ed2c47f68
JuZED <or.n.juz@gmail.com>
2014-08-17 11:25:24 +0200

    New version 1.1

--- wp-fullpage.php
+++ wp-fullpage.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP FullPage
  * Plugin URI: http://www.juzed.fr/
  * Description: Change your WordPress website to a Fullscreen Scrolling one using fullPage.js
- * Version: 1.0 
+ * Version: 1.1 
  * Author: Julien Zerbib
  * Author URI: http://www.juzed.fr/
  * 
