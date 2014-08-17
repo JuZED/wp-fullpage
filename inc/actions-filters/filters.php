fc14025008edaf4e3527a41902143fbd54e2eb77
JuZED <or.n.juz@gmail.com>
2014-08-17 11:25:12 +0200

    Adding fullpage pages to front page dropdown pages

--- inc/actions-filters/filters.php
+++ inc/actions-filters/filters.php
@@ -27,7 +27,41 @@ class WP_Fullpage_Filters extends WP_Fullpage_Base {
 	 */
 	public function filters() {
 
+		add_filter( 'wp_dropdown_pages', array( &$this, 'wp_dropdown_pages' ) );
+
 	} // END public function filters
+
+	/**
+	 * Dropdown Pages Filter to add Fullpage Post Type pages to "Settings / Reading" Page
+	 *
+	 * @param   string  $output  
+	 *
+	 * @return  string           
+	 */
+	public function wp_dropdown_pages( $output ) {
+		
+		// Avoid other page than "Settings / Reading" to be modified
+		if( ! strpos( $output, "name='page_on_front'" ) || ! strpos( $output, "<option value=\"0\">" . __( '&mdash; Select &mdash;' ) . "</option>" ) )
+			return $output;
+
+		$fullpages = get_posts( array(
+			'post_type'      => 'fullpage',
+			'posts_per_page' => -1,
+			'orderBy'        => 'title',
+			'order'          => 'ASC',
+		) );
+
+		$args = array(
+			'child_of' => 0,
+			'selected' => get_option( 'page_on_front' ),
+			'echo'     => 0,
+		);
+
+		extract( $args, EXTR_SKIP );
+
+		return str_replace( '</select>', walk_page_dropdown_tree( $fullpages, 0, $args ) . '</select>', $output );
+		
+	} // END public function init_textdomain
 
 } // END class WP_Fullpage_Filters
 
